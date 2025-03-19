72b15d0bdd9830003ec5b1cc1696c862c8042c67
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2025-03-19 22:54:58 +0100

    Delete scrapper.sh
    Delete the first one that was out of the folder

--- scrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATA=$(curl "https://www.google.com/finance/quote/TSLA:NASDAQ")
-PRICE=$(echo "$DATA" | grep -oP '(\d+\.\d+)(?=\s*&nbsp;\$)' )
-TIME=$(date "+%Y-%m-%d %H:%M:%S")
-
-echo "$TIME;$PRICE" >> ../docs/data.csv
