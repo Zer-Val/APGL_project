a92469ed5d16ce98d0e9631a82ff2a1cfaa14fa2
ZerVal <tank.delagrd@gmail.com>
2025-03-23 15:41:41 +0100

    Corrected a typo and imprived app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 app = Flask(__name__) # Create the Flask application
 
+# This function is used to load the data from the CSV file
 def load_data():
 	try:
 		df = pd.read_csv("/home/ubuntu/APGL_projet/docs/data.csv", sep=";", encoding="utf-8")
@@ -24,6 +25,21 @@ def load_report_data():
     except FileNotFoundError:
         print("CSV file not found")
         df = pd.DataFrame({"opening_value": [0], "min_value": [0], "max_value": [0], "closing_value": [0], "volatility": [0]})
+    return df
+
+def load_history():
+    try:
+        df = pd.read_csv("/home/ubuntu/APGL_projet/docs/data_history.csv", sep=";", encoding="utf-8")
+        df.columns=["date", "opening_value", "min_value", "max_value",  "closing_value", "volatility"]
+        columns_to_convert = ["opening_value", "min_value", "max_value", "closing_value", "volatility"]
+
+        for col in columns_to_convert:
+            df[col] = pd.to_numeric(df[col].astype(str).str.replace(',', '.'), errors='coerce')
+
+    except FileNotFoundError:
+        print("CSV file not found")
+        df = pd.DataFrame({"date": [], "opening_value": [], "min_value": [], "max_value": [], "closing_value": [], "volatility": [] })
+
     return df
 
 @app.route('/')
@@ -48,17 +64,19 @@ def index():
                 gridcolor="gray"                
 			),
 			yaxis=dict(
-				tickfont=dict(color='black', family='Arial, sans-serif', size=14, weight='bold'),   
+				tickfont=dict(color='black', family='Arial, sans-serif', size=14, weight='bold'),
 				showgrid=True,
 				gridcolor="gray"
 			),
 			plot_bgcolor="rgba(255, 255, 255, 1)",
-			paper_bgcolor="rgba(255, 255, 255, 1)",  
+			paper_bgcolor="rgba(255, 255, 255, 1)", 
 			font=dict(family="Arial, sans-serif", size=14, color="black"), 
-			hovermode="closest", 
+			hovermode="closest",
 		)
 		
+	# Generate the plotly graph in HTML format
 	graphHTML = pio.to_html(fig, full_html=False)
+	# Return the HTML template with the graph
 	return render_template('index.html',graphHTML=graphHTML)
 
 @app.route('/data')
@@ -90,7 +108,25 @@ def get_rapport():
         rapport_python={ "opening_value": 0, "min_value": 0, "max_value": 0, "closing_value": 0, "volatility": 0 }
         return  jsonify(rapport_python)
 
+@app.route('/get-server-time')
+def get_server_time():
+    server_time = datetime.now().isoformat()
+    return jsonify({"server_time": server_time})
+
+@app.route('/history')
+def history():
+    df = load_history()
+    graphs = {}
+    df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d", errors="coerce").dt.date
+    columns=["opening_value", "min_value", "max_value", "closing_value", "volatility"]
+    for col in columns:
+        fig = px.line(df, x="date", y=col, title=f"Evolution de {col.replace('_',' ').capitalize()}")
+        fig.update_layout(template="plotly_dark")
+        fig.update_xaxes(type='category')
+        fig.update_traces(line=dict(color='red'))  
+        graphs[col] = pio.to_html(fig, full_html=False)
+
     return render_template('history.html', graphs=graphs)
 
-if __name__ == "__main__": # If the script is executed directly
-    app.run(debug=True, host="0.0.0.0", port=5000) # Start the web server at port 5000
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
