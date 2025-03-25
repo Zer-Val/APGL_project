1f0e0865ec3b14da265ecfd0e026f11566fdb61f
ZerVal <tank.delagrd@gmail.com>
2025-03-25 10:54:50 +0100

    Added comments to the app.py file

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,35 +6,44 @@ from datetime import datetime
 
 app = Flask(__name__) # Create the Flask application
 
-# This function is used to load the data from the CSV file
+# This function is used to load the data from a CSV file for the plot of the time serie
 def load_data():
-	try:
-		df = pd.read_csv("/home/ubuntu/APGL_projet/docs/data.csv", sep=";", encoding="utf-8")
+
+	try: 
+		df = pd.read_csv("/home/ubuntu/APGL_projet/docs/data.csv", sep=";", encoding="utf-8") #Load the data from the csv file
 		df.columns = ["timestamp", "value"]
-		df["timestamp"] = pd.to_datetime(df["timestamp"], format="%Y-%m-%d %H:%M:%S", errors="coerce")
-		df["value"] = pd.to_numeric(df["value"].astype(str).str.replace(',', '.'), errors='coerce')
+		df["timestamp"] = pd.to_datetime(df["timestamp"], format="%Y-%m-%d %H:%M:%S", errors="coerce") #Makes sure we have a certain time format
+		df["value"] = pd.to_numeric(df["value"].astype(str).str.replace(',', '.'), errors='coerce') # Replace the comma and turn the string to a number 
+
 	except FileNotFoundError:
 		print("CSV file not found")
 		df = pd.DataFrame({"timestamp": [], "value": []})
+
 	return df
 
+# This function is used to load the data from a CSV file for the daily report
 def load_report_data():
+
     try:
-	    df = pd.read_csv("/home/ubuntu/APGL_projet/docs/rapport.csv", sep=";", encoding="utf-8")
+	    df = pd.read_csv("/home/ubuntu/APGL_projet/docs/rapport.csv", sep=";", encoding="utf-8") #Load the data from the csv file
 	    df.columns = ["opening_value", "min_value", "max_value", "closing_value", "volatility"]
+
     except FileNotFoundError:
         print("CSV file not found")
         df = pd.DataFrame({"opening_value": [0], "min_value": [0], "max_value": [0], "closing_value": [0], "volatility": [0]})
+
     return df
 
+# This function is used to load the data from a CSV file for the historical key values time series
 def load_history():
-    try:
-        df = pd.read_csv("/home/ubuntu/APGL_projet/docs/data_history.csv", sep=";", encoding="utf-8")
-        df.columns=["date", "opening_value", "min_value", "max_value",  "closing_value", "volatility"]
-        columns_to_convert = ["opening_value", "min_value", "max_value", "closing_value", "volatility"]
 
+    try:
+        df = pd.read_csv("/home/ubuntu/APGL_projet/docs/data_history.csv", sep=";", encoding="utf-8") #Load the data from the csv file
+        df.columns=["date", "opening_value", "min_value", "max_value",  "closing_value", "volatility"]
+        df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d", errors="coerce").dt.date # Makes sure we have a certain time format
+        columns_to_convert = ["opening_value", "min_value", "max_value", "closing_value", "volatility"]
         for col in columns_to_convert:
-            df[col] = pd.to_numeric(df[col].astype(str).str.replace(',', '.'), errors='coerce')
+            df[col] = pd.to_numeric(df[col].astype(str).str.replace(',', '.'), errors='coerce') # Replace the comma and turn the string to a number
 
     except FileNotFoundError:
         print("CSV file not found")
@@ -42,15 +51,20 @@ def load_history():
 
     return df
 
-@app.route('/')
+
+@app.route('/') # Main page of the dashboard, where the time serie and the actual stock value of Tesla
 def index():
+
+    # Get the actual time and the time limits for the day
 	current_time = datetime.now()
 	start_time = current_time.replace(hour=20, minute=0, second=0, microsecond=0)
 	end_time = current_time.replace(hour=9, minute=30, second=0, microsecond=0)
-	df = load_data()
+
+	df = load_data() # Used the previously defined function
 
 	if df.empty or df["value"].isna().all():
 		fig = px.line(title="No data available")
+
 	else:
 		fig = px.line(df, x="timestamp", y="value")
 		fig.update_traces(line=dict(color='purple'))
@@ -59,7 +73,7 @@ def index():
 			yaxis_title='',
 			xaxis=dict(
 				tickformat="%H:%M:%S",
-				tickfont=dict(color='black', family='Arial, sans-serif', size=14, weight='bold'),
+				tickfont=dict(color='black', family='Arial, sans-serif', size=14, weight='bold'), 
                 showgrid=True,
                 gridcolor="gray"                
 			),
@@ -68,18 +82,19 @@ def index():
 				showgrid=True,
 				gridcolor="gray"
 			),
-			plot_bgcolor="rgba(255, 255, 255, 1)",
-			paper_bgcolor="rgba(255, 255, 255, 1)", 
-			font=dict(family="Arial, sans-serif", size=14, color="black"), 
-			hovermode="closest",
+			plot_bgcolor="rgba(255, 255, 255, 1)",  # Background color of the plot area (white)
+			paper_bgcolor="rgba(255, 255, 255, 1)",  # Background color of the paper (white)
+			font=dict(family="Arial, sans-serif", size=14, color="black"),
+			hovermode="closest", # Change the hovermode to closest (ie the hover data is displayed for the closest point)
 		)
-		
+ 		
 	# Generate the plotly graph in HTML format
 	graphHTML = pio.to_html(fig, full_html=False)
 	# Return the HTML template with the graph
 	return render_template('index.html',graphHTML=graphHTML)
 
-@app.route('/data')
+
+@app.route('/data') # Page not supposed to be accessed by the dashboard users, give access to the data.csv for the JS
 def get_data():
 
 	try:
@@ -89,12 +104,15 @@ def get_data():
 			"values": df["value"].tolist()
 		}
 		return jsonify(data)
+
 	except Exception as e:
 		print("Erreur:", e)
 		return jsonify({"error": "Unable to load data"}), 500
 
-@app.route('/rapport_tesla')
+
+@app.route('/rapport_tesla') # Page not supposed to be accessed by the dashboard users, give access to the rapport_tesla.csv for the JS
 def get_rapport():
+
     try:
         df = load_report_data()
         if (df.empty or len(df)==0):
@@ -108,19 +126,21 @@ def get_rapport():
         rapport_python={ "opening_value": 0, "min_value": 0, "max_value": 0, "closing_value": 0, "volatility": 0 }
         return  jsonify(rapport_python)
 
-@app.route('/get-server-time')
+
+@app.route('/get-server-time') # Page not supposed to be accessed by the dashboad users, give access to the server time
 def get_server_time():
     server_time = datetime.now().isoformat()
     return jsonify({"server_time": server_time})
 
-@app.route('/history')
+
+@app.route('/history') # Secondary page where the historical key values time series are plotted
 def history():
     df = load_history()
     graphs = {}
     df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d", errors="coerce").dt.date
     columns=["opening_value", "min_value", "max_value", "closing_value", "volatility"]
     for col in columns:
-        fig = px.line(df, x="date", y=col, title=f"Evolution de {col.replace('_',' ').capitalize()}")
+        fig = px.line(df, x="date", y=col, title=f"Evolution of the {col.replace('_',' ').capitalize()}")
         fig.update_layout(template="plotly_dark")
         fig.update_xaxes(type='category')
         fig.update_traces(line=dict(color='red'))  
@@ -128,5 +148,5 @@ def history():
 
     return render_template('history.html', graphs=graphs)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__": # If the script is executed directly
+    app.run(debug=True, host="0.0.0.0", port=5000) # Start the web server at port 5000 (http://XX.XX.XX.XX:5000)
